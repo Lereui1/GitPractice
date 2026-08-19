7631592ca109441a110a4e5db656f6445186cf6c
Dmitry <casyyycas.yyy@gmail.com>
2026-08-19 15:43:38 +0300

    Update welcome message version 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, Git! Welcome!");
+        System.out.println("Hello, Git! Welcome to my project!");
 
     }
 }
